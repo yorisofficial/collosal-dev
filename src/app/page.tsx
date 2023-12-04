2a2ae4beb0c0b281304a24497c1e98cde7ca0bf8
ADE YOGA WIYADNYANA <yorisofficial@gmail.com>
2023-12-04 23:44:26 +0800

    first upload

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="hero-section relative -z-10 flex h-screen w-full items-center justify-center">
+        <div className="absolute z-10 flex h-screen w-full flex-col items-center justify-center gap-4">
+          <h1 className="text-4xl font-semibold capitalize">lorem title</h1>
+          <p className="w-3/4 text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            expedita necessitatibus rem dolorem, sequi quas id eaque omnis,
+            debitis architecto, exercitationem consequatur aspernatur facere
+            nesciunt? Hic, consequatur tenetur quos debitis iure magnam dolore
+            fugit distinctio ullam aliquid optio, aliquam architecto inventore
+            eligendi labore dolores voluptatibus dicta, dolor molestias corrupti
+            sed?
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
